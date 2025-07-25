e78b1c14ef62cb6b8bbf153e5567c8a15bf590a1
SooY2 <ssy67366736@gmail.com>
2025-07-25 15:13:11 +0900

    feat: add emoji

---
+++ default-template/js/emoji.js
@@ -0,0 +1,14 @@
+
+const section = document.getElementById('emoji-container');
+
+for (let i = 1; i <= 2376; i++) {
+const img = document.createElement('img');
+img.src = `../assets/emoji/image${i}.png`;
+img.alt = `emoji${i}`;
+// img.loading = 'lazy'; // 최적화: 화면에 보일 때 로드
+img.style.width = '32px';  // 필요 시 크기 조절
+img.style.height = '32px';
+img.style.margin = '2px';
+
+section.appendChild(img);
+}
